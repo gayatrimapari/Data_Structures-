e0575b5d62ce3af297996fb14a20f8b0d10453d2
gayatrimapari <gayatrimapari30@gmail.com>
2024-06-07 16:53:23 +0530

    array.cpp

--- array.cpp
+++ array.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"enter count";
+  cin>>n;
+  int arr[n] ={ };
 
+  for(int i=0; i<n; i++){
+    cout<<"enter value : ";
+    cin>>arr[i];
+  }
+  
+  cout<<"array is : ";
+  
+   for(int i=0; i<n; i++){
+    cout<<" "<<arr[i];
+   }
+  cout<<" }";
+
+  return 0;
+}
